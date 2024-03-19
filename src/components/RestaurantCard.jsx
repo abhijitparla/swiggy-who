ed2cca8d7815308914b042f8e8fc10bd25b7b11f
Abhijit Parlapalli <abhijit.parla01@gmail.com>
2024-03-19 22:55:35 +0530

    initial commit

---
+++ src/components/RestaurantCard.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const RestaurantCard = ({restaurantName, imgId, rating, avgTime, cuisine, location}) => {
+  const val = "e0839ff574213e6f35b3899ebf1fc597"
+  return (
+    <div className='rest-card'>
+      <div className='rest-img-cls'>
+        <img src={`https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/`+imgId} alt="" />
+      </div>
+      <div className='rest-card-items'>
+        <h3>{restaurantName}</h3>
+        <p>Rating: {rating} {avgTime}</p>
+        <p className='cuisine-cls'>{cuisine}</p>
+        <p>{location}</p>
+      </div>
+    </div>
+  )
+}
+
+export default RestaurantCard
